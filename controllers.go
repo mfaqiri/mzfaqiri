a5a5d688e018a3a5c26de62f003068ddcb9ea4cb
Mansoor Faqiri <mzfaqiri@gmail.com>
2024-08-05 15:26:45 -0400

    made things look prettier

--- controllers.go
+++ controllers.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
-	"strconv"
+    "fmt"
 
 	"github.com/a-h/templ"
 	"github.com/gorilla/mux"
@@ -14,59 +13,55 @@ import (
 type technology struct {
   Name string
   Summary string
-  BulletPoints []string
+  Usage string
 }
 
 var technologies = []technology {
   {
-    Name: "git",
+    Name: "GIT",
     Summary: "A version control system used in many projects to keep track of changes within the code, " +
             "collaborate with other developers, and sometimes revert changes entirely",
-    BulletPoints: []string{
-          "I use this technology constantly in my personal development.",
-          "I have written scripts to automate some git actions",
-          "3+ years of professional experience",
-          "In many situations I have created a git repo, or gave recommendations in the creation of a git repo",
-          "In a professional setting I make changes, and pull requests with git to present for a code review",
-          "In a professional setting I have been asked to prepare branches for releasing a new version of our product"},
+
+    Usage: "This is my default choice for a VCS (Version Control System), I use this for whatever code base I am in even the one for this very website.",
   },
   {
     Name: "C#",
     Summary: "An OOP language developed by Microsoft that is usually developed with a JIT (Just In Time) compiler" +
              ", to develop code in a variety of situations such as API/Web, IoT, Mobile/Desktop, Machine Learning," +
              "and Game development",
-    BulletPoints: []string{
-          "I have 3+ years of professional experience maintaining a deployed C# API application",
-          "I have migrated a codebase from .NET Framework 4.6.2 -> 4.8 and then to .NET Core 6",
-          "I have mentored colleagues on how to develop in our C# codebase",
-          "I have worked on our deployment methods to bring it up to date with .NET 6"},
+
+    Usage : "I primarily would use C# for API development, something that I feel needs a more robust codebase to maintain and add features",
   },
   {
-    Name: "bash",
+    Name: "BASH",
     Summary: "A POSIX compliant shell used to interface with linux/unix environments",
-    BulletPoints: []string{
-          "I have used this extensively on my home linux machines",
-          "Used to automate build processes professionally, and in personal projects",
-          "Used to interface with remote machines using SSH, to do anything I may need to do remotely",
-          "Experience configuring and setting environment variables"},
+
+    Usage: "I use this shell to navigate my personal machines, make changes in the file system. Sometimes I may make a script to automate a large command I use often.",
   },
   {
-    Name: "Docker",
+    Name: "DOCKER",
     Summary: "Docker is a platform for developing, shipping, and running applications. It is very useful as you do " +
           "not need to worry about the environment you are deploying to, docker will be setup with all the tooling " +
           "you have configured, and can test is working locally.",
-    BulletPoints: []string{
-          "I use docker in my personal projects, where I have to deploy to another environment",
-          "I have experience setting up a docker node, with required environment details and setting up github actions to deploy when I push to my main branch"},
+
+    Usage: "I primarily use Docker to create applications where I would like to have a virtual environment without much access to files outside of what the application needs. This web application is built and deployed with Docker.",
+  },
+  {
+    Name: "GOLANG",
+    Summary: "An open source programming language created by GOOGLE",
+
+    Usage: "This website is the first application I have built with go, and it is a language I have taken a great liking to. It has a robust set of features built with it, and with the help of templ I have a more integrated experience using html with the benefits of golang",
   },
 }
 
-var home_page_paragraph = "Hello, welcome to my website." +
-                          "The purpose of this site is to " +
-                          "present my development abilities " +
-                          "to those interested in contacting " +
-                          "me my email is linked below, as well " +
-                          "as my github."
+var home_page_paragraph = "Hello, my name is Mansoor Faqiri and I am a Full Stack Developer."+
+                            " I am aiming to become an entry level CyberSecurity professional, I am CompTIA Security + certified,"+
+                            " proof of this is in the certifications tab, in the form of an embedded badge."+
+                            " Other than this certification I am also personally studying various cybersecurity concepts through HackTheBox."+
+                            " If you are interested in my professional experience as a developer, you may check my resume tab for a downloadable PDF of my resume, with work experience."+
+                            " To see when I am available, you can check my calendar for my availability. To see how to message me, details are on my resume and below are links to my github and a mailto link to my email,"+
+                            " if you have an email provider configured to open such links e.g. Outlook, Thunderbird, etc."+
+                            " Thank you for visiting my site"
 
 func main() {
 
@@ -80,24 +75,7 @@ func main() {
   r.Handle("/home", templ.Handler(home_page(home_page_paragraph)))
   r.Handle("/resume", templ.Handler(resume_page()))
   r.Handle("/certifications", templ.Handler(certification_page()))
-  r.Handle("/technology", templ.Handler(technology_page(technologies, 0)))
-  r.HandleFunc("/technology/{techInd}", func(w http.ResponseWriter, r *http.Request) {
-    vars := mux.Vars(r)
-    techInd,err := strconv.Atoi(vars["techInd"])
-
-    if err != nil {
-      log.Fatal(err)
-
-    }  else {
-
-        log.Print(technologies[techInd])
-
-        technology_page_summary(technologies[techInd]).Render(r.Context(), w)
-
-      }
-
-  })
-
+  r.Handle("/technology", templ.Handler(technology_page(technologies)))
   r.Handle("/static/js/htmx.min.js", http.StripPrefix("/static",fs))
   r.Handle("/static/css/skeleton.css", http.StripPrefix("/static",fs))
   r.Handle("/static/images/email.svg", http.StripPrefix("/static",fs))
@@ -108,7 +86,6 @@ func main() {
   r.Handle("/static/images/site.webmanifest", http.StripPrefix("/static",fs))
 
   r.Handle("/calendar", templ.Handler(calendar_page()))
-  r.Handle("/about-me", templ.Handler(about_me_page()))
 
   port := ":" + os.Getenv("PORT")
 
--- go.mod
+++ go.mod
@@ -2,6 +2,6 @@ module mzfaqiri.com/controllers
 
 go 1.22.4
 
-require github.com/a-h/templ v0.2.707
+require github.com/a-h/templ v0.2.747
 
 require github.com/gorilla/mux v1.8.1
